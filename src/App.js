925675e45e98e972b8208faa91f12dd6169129b1
Alex Martin <atonymartin20@yahoo.com>
2022-01-28 16:43:30 -0600

    index.js and App.js files completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './components/homepage';
+import Crew from './components/crew';
+import Destination from './components/destination';
+import Tech from './components/tech';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className='app-outside-div'>
+				<Routes>
+					<Route exact path='/' element={<Homepage />} />
+					<Route path='/tech' element={<Tech />} />
+					<Route path='/crew' element={<Crew />} />
+					<Route path='/destination' element={<Destination />} />
+					<Route path='*' element={<Homepage />} />
+				</Routes>
+			</div>
+		);
+	}
 }
 
 export default App;
